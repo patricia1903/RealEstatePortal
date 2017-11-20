71a34899dc9bfa56b93279b4a2d787e774a30937
PatriciaArdelean <PatriciaArdelean>
2017-11-20 23:19:17 +0200

    ListView from Android modified to work with CustomAdapter

--- RealEstatePortalAndroid/app/src/main/java/com/realestateportalandroid/activity/PropertyListActivity.java
+++ RealEstatePortalAndroid/app/src/main/java/com/realestateportalandroid/activity/PropertyListActivity.java
@@ -1,10 +1,17 @@
 package com.realestateportalandroid.activity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.realestateportalandroid.R;
 import com.realestateportalandroid.model.Property;
@@ -19,6 +26,7 @@ public class PropertyListActivity extends AppCompatActivity {
     List<Property> properties;
 
     ListView listView;
+    PropertyListAdapter arrayAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,12 +38,42 @@ public class PropertyListActivity extends AppCompatActivity {
 
         listView = findViewById(R.id.listView);
 
-        final ArrayAdapter<Property> arrayAdapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, properties);
-
+        arrayAdapter = new PropertyListAdapter(this, properties);
         listView.setAdapter(arrayAdapter);
+    }
 
 
+    private class PropertyListAdapter extends ArrayAdapter<Property> {
+        private List<Property> properties;
+        private Context context;
+
+        public PropertyListAdapter(Context context, List<Property> properties) {
+            super(context, -1, properties);
+            this.properties = properties;
+            this.context = context;
+        }
+
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View rowView = inflater.inflate(R.layout.row_propertylist, parent, false);
+
+            Property property = getItem(position);
+
+            TextView textView = rowView.findViewById(R.id.firstLine);
+            textView.setText(property.getTitle());
+
+            TextView textView2 = rowView.findViewById(R.id.secondLine);
+            textView2.setText(property.getPrice());
+
+
+            ImageView imageView = rowView.findViewById(R.id.icon);
+            imageView.setImageResource(R.drawable.re);
+
+            return rowView;
+        }
     }
 
 
